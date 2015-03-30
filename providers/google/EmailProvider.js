e105984ad02bcb67fdb19467ac3844589389ed55
David Karam <dskaram@gmail.com>
2015-03-30 13:43:35 +0200

    Ensure emails are sorted by date

--- providers/google/EmailProvider.js
+++ providers/google/EmailProvider.js
@@ -1,12 +1,14 @@
 define([
   "underscore",
   "backbone",
+  "moment",
   "providers/Provider",
   "providers/ProviderEntry",
   chrome.extension.getURL("/providers") + "/google/GoogleAPIWrapper.js"
 ], function(
   _,
   Backbone,
+  moment,
   Provider,
   ProviderEntry,
   GoogleAPIWrapper
@@ -33,23 +35,30 @@ define([
 
       } else {
         this._googleWrapper.mail.unread().done(function(response) {
-          response= response.result;
-          var threads= _.groupBy(response, function(r) { return r.result.threadId; });
+            response= response.result;
+            var sorted= new Backbone.Collection();
+            sorted.comparator= function(a, b) {
+              return a.get("date").isBefore(b.get("date")) ? 1 : -1;
+            };
 
-          var list= _.map(threads, function(messages, threadId) {
-            var lastMessage= messages[messages.length - 1];
-            var subject= _.filter(lastMessage.result.payload.headers, function(entry) { return entry.name === "Subject"; });
-            var from= _.filter(lastMessage.result.payload.headers, function(entry) { return entry.name === "From"; })[0].value;
-            from= from.substring(0, from.indexOf("<") - 1);
-            var label= subject[0] ? subject[0].value : lastMessage.result.snippet;
+            var threads= _.groupBy(response, function(r) { return r.result.threadId; });
+            _.each(threads, function(messages, threadId) {
+              var lastMessage= messages[messages.length - 1];
+              var subject= _.filter(lastMessage.result.payload.headers, function(entry) { return entry.name === "Subject"; });
+              var from= _.filter(lastMessage.result.payload.headers, function(entry) { return entry.name === "From"; })[0].value;
+              from= from.substring(0, from.indexOf("<") - 1);
 
-            return new ProviderEntry({
-              label: from + " " + label + (messages.length > 1 ? " - (" + messages.length + ")" : ""),
-              url: "https://mail.google.com/mail/u/0/#inbox/" + threadId
-            });
+              var date= new moment(_.filter(lastMessage.result.payload.headers, function(entry) { return entry.name === "Date"; })[0].value);
+              var label= subject[0] ? subject[0].value : lastMessage.result.snippet;
+
+              sorted.add(new ProviderEntry({
+                label: from + " " + label + (messages.length > 1 ? " - (" + messages.length + ")" : ""),
+                date: date,
+                url: "https://mail.google.com/mail/u/0/#inbox/" + threadId
+              }));
           });
 
-          result.resolve(new Backbone.Collection(list));
+          result.resolve(sorted);
         });
       }
 
